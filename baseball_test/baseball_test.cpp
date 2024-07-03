8dadda61db55f78912ecf875fb0e33a7890eb9f7
j1.woo <j1.woo@samsung.com>
2024-07-03 15:31:25 +0900

    textfixture

--- baseball_test/baseball_test.cpp
+++ baseball_test/baseball_test.cpp
@@ -4,6 +4,12 @@
 class BaseBallTest : public testing::Test {
 public:
     Baseball game{ "123" };
+    void compareResult(GameResult answer, GameResult expected)
+    {
+        EXPECT_EQ(answer.solved, expected.solved);
+        EXPECT_EQ(answer.strikes, expected.strikes);
+        EXPECT_EQ(answer.balls, expected.balls);
+    }
 };
 
 TEST_F(BaseBallTest, PreCondition000) {
@@ -22,32 +28,24 @@ TEST_F(BaseBallTest, PreCondition002) {
 TEST_F(BaseBallTest, Logic000) {
     GameResult answer = game.guess("123");
     GameResult expected = { true, 3, 0 };
-    EXPECT_EQ(answer.solved, expected.solved);
-    EXPECT_EQ(answer.strikes, expected.strikes);
-    EXPECT_EQ(answer.balls, expected.balls);
+    compareResult(answer, expected);
 }
 
 TEST_F(BaseBallTest, Logic001) {
     GameResult answer = game.guess("456");
     GameResult expected = { false, 0, 0 };
-    EXPECT_EQ(answer.solved, expected.solved);
-    EXPECT_EQ(answer.strikes, expected.strikes);
-    EXPECT_EQ(answer.balls, expected.balls);
+    compareResult(answer, expected);
 }
 
 TEST_F(BaseBallTest, Logic002) {
     GameResult answer = game.guess("923");
     GameResult expected = { false, 2, 0 };
-    EXPECT_EQ(answer.solved, expected.solved);
-    EXPECT_EQ(answer.strikes, expected.strikes);
-    EXPECT_EQ(answer.balls, expected.balls);
+    compareResult(answer, expected);
 }
 
 TEST_F(BaseBallTest, Logic003) {
     GameResult answer = game.guess("321");
     GameResult expected = { false, 1, 2 };
-    EXPECT_EQ(answer.solved, expected.solved);
-    EXPECT_EQ(answer.strikes, expected.strikes);
-    EXPECT_EQ(answer.balls, expected.balls);
+    compareResult(answer, expected);
 }
 
